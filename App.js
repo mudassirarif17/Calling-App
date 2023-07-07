52690a54d5d70faf992b63d14609d0a1805906c3
mudassirarif17 <mudassirinoxent@gmail.com>
2023-07-07 15:34:29 +0500

    bugs in the code

--- App.js
+++ App.js
@@ -1,24 +1,40 @@
-// In App.js in a new project
-
 import React , {useState} from 'react';
-import { View, Text, TextInput, Button , TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { View, Text, TextInput, Button , TouchableOpacity , Alert } from 'react-native';
+import { NavigationContainer , useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CallingSreen from "./CallingScreen"
+
 
 function HomeScreen() {
   // Function for making a random meeting id 
+  const [userName , setUserName] = useState('');
+  const [randomId , setRandomId] = useState('');
+
+  const navigation = useNavigation();
+
   const generateRandomId = () =>{
     return `${Math.floor(Math.random() * 10000)}-${Math.floor(Math.random() * 10000)}-${Math.floor(Math.random() * 10000)}`
   }
 
-  const [randomId , setRandomId] = useState("");
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <View style={{width : "90%"}}>
-        <TextInput value={randomId} onChange={(text)=>setRandomId(text)} style={{borderWidth : 1 , borderColor : "black" , marginBottom: 20}} />
 
-        <Button title='Join Meeting' />
+        <TextInput value={userName} placeholder={"Enter Your Name"} onChange={(text)=>setUserName(text)} style={{borderWidth : 1 , borderColor : "black" , marginBottom: 20}} />
+
+        <TextInput value={randomId} placeholder={"Enter Meeting Id"} onChange={(text)=>setRandomId(text)} style={{borderWidth : 1 , borderColor : "black" , marginBottom: 20}} />
+
+        <Button title='Join Meeting' onPress={()=>{
+          if(randomId.length > 5 && 
+            userName.length >= 5 && 
+            userName.indexOf(' ') == -1){
+            navigation.navigate("CallingSreen" , {callId: randomId , name: userName} );
+          }
+          else{
+            Alert.alert("Enter Valid Id or Valid User Name" , userName);
+          }
+        }} />
 
         <TouchableOpacity onPress={()=>{
           const id = generateRandomId();
@@ -39,6 +55,9 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="CallingSreen" component={CallingSreen} options={{
+          headerShown : false
+        }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- CallingScreen.js
+++ CallingScreen.js
@@ -1,13 +1,24 @@
-// App.js
 import React, { Component } from 'react';
 import {StyleSheet , View} from "react-native" ;
-import {ZegoUIKitPrebuiltCall, ONE_ON_ONE_VIDEO_CALL_CONFIG } from '@zegocloud/zego-uikit-prebuilt-call-rn'
+// This line is for one to one call 
+// import {ZegoUIKitPrebuiltCall, ONE_ON_ONE_VIDEO_CALL_CONFIG } from '@zegocloud/zego-uikit-prebuilt-call-rn';
+
+// This line is for group call
+import {ZegoUIKitPrebuiltCall, GROUP_VIDEO_CALL_CONFIG } from '@zegocloud/zego-uikit-prebuilt-call-rn';
+
+import { useRoute , useNavigation } from '@react-navigation/native';
 
 export default function VoiceCallPage(props) {
     const userId = String(Math.floor(Math.random() * 100000));
-
-    console.log("id" , userId);
+    // console.log("id" , userId);
     
+    const route =  useRoute(); 
+    const {callId , name} = route.params;
+    // console.log(userName);
+    // console.warn(userName);
+
+    const navigation = useNavigation();
+
     return (
         <View style={styles.container}>
             <ZegoUIKitPrebuiltCall
@@ -17,16 +28,25 @@ export default function VoiceCallPage(props) {
                 userID={userId}   // userID can be something like a phone number or the user id on your own user system. 
                 // userName={"user_12345"}
                 userName={`user_${userId}`}
-                callID={"group123"}     // callID can be any unique string. 
+                // userName={userName}
+                //callID={"group123"}     // callID can be any unique string. 
+                callID={callId}     // callID can be any unique string. 
 
                 config={{
                     // You can also use ONE_ON_ONE_VOICE_CALL_CONFIG/GROUP_VIDEO_CALL_CONFIG/GROUP_VOICE_CALL_CONFIG to make more types of calls.
-                    ...ONE_ON_ONE_VIDEO_CALL_CONFIG,
+
+                    // For VideoCall
+                    ...GROUP_VIDEO_CALL_CONFIG,
+
+                    // For One To one Call
+                    // ...ONE_ON_ONE_VIDEO_CALL_CONFIG,
                     onOnlySelfInRoom: () => { 
                         // props.navigation.navigate('HomePage') 
+                        navigation.goBack(); 
                     },
                     onHangUp: () => { 
-                        // props.navigation.navigate('HomePage') 
+                        // props.navigation.navigate('HomePage')
+                        navigation.goBack(); 
                     },
                 }}
             />
